3629dfe7404c68ddeafb45129e5ab47e9c3c86ee
Guosc <38343603+Niceguosc@users.noreply.github.com>
2018-05-29 15:20:04 +0800

    Update denglu.php

--- denglu/denglu.php
+++ denglu/denglu.php
@@ -3,17 +3,20 @@
 	$MyUser = 'guogege';
     $Getpassword = $_POST['pass'];
     $MyPass = 'guogege';
-	if($GetUsername==$MyUser){
-		echo "用户名正确";
-	}else{
-		echo '用户名不存在';
+    if(!$_POST['user']|| !$_POST['pass']){
+     	echo"登录信息不能为空";
+   }else{
+		if($GetUsername==$MyUser&&$Getpassword==$MyPass){
+			echo "用户名正确密码正确";
+		}else if($GetUsername==$MyUser&&$Getpassword!=$MyPass){
+			echo '密码错误';
 
-    }
-    if($Getpassword==$MyPass){
-		echo '密码正确';
+	    }
+	    else if($GetUsername!=$MyUser&&$Getpassword==$MyPass){
+			echo '用户名不存在';
 
-	}else{
-		echo '密码错误';
-
-	}
+		}else if($GetUsername!=$MyUser&&$Getpassword!=$MyPass){
+	       echo '请输入正确的用户名和密码';
+		}
+  }
 ?>
