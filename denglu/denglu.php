b539f6b9c3cd6737c521f0a4fe8fb974dadbf723
Guosc <38343603+Niceguosc@users.noreply.github.com>
2018-05-31 07:58:42 +0800

    Update denglu.php

--- denglu/denglu.php
+++ denglu/denglu.php
@@ -1,22 +1,19 @@
 <?php
 	$GetUsername = $_POST['user'];
-	$MyUser = 'guogege';
-    $Getpassword = $_POST['pass'];
-    $MyPass = 'guogege';
-    if(!$_POST['user']|| !$_POST['pass']){
-     	echo"登录信息不能为空";
-   }else{
-		if($GetUsername==$MyUser&&$Getpassword==$MyPass){
-			echo "用户名正确密码正确";
-		}else if($GetUsername==$MyUser&&$Getpassword!=$MyPass){
-			echo '密码错误';
-
-	    }
-	    else if($GetUsername!=$MyUser&&$Getpassword==$MyPass){
-			echo '用户名不存在';
-
-		}else if($GetUsername!=$MyUser&&$Getpassword!=$MyPass){
-	       echo '请输入正确的用户名和密码';
-		}
-  }
+	$Getpassword = $_POST['password'];
+		$arr = array( 
+			array('name'=>'guogege','url'=>'123456'), 
+			array('name'=>'zixin','url'=>'321321'), 
+			array('name'=>'leng','url'=>'45678'), 
+			array('name'=>'dada','url'=>'hu123')
+		);
+		foreach($arr as $key=>$val){ 
+		  	if($GetUsername==$val["name"] && $Getpassword==$val["url"]){
+				echo '注册成功';
+				return;
+			}else{
+				echo '注册失败';
+				return;
+			}
+		}	
 ?>
