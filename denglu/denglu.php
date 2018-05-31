4877f0cd08487456f379624503370cdd5a8b9fd3
Guosc <38343603+Niceguosc@users.noreply.github.com>
2018-05-31 09:41:33 +0800

    Update denglu.php

--- denglu/denglu.php
+++ denglu/denglu.php
@@ -1,19 +1,37 @@
 <?php
-	$GetUsername = $_POST['user'];
-	$Getpassword = $_POST['password'];
-		$arr = array( 
-			array('name'=>'guogege','url'=>'123456'), 
-			array('name'=>'zixin','url'=>'321321'), 
-			array('name'=>'leng','url'=>'45678'), 
-			array('name'=>'dada','url'=>'hu123')
-		);
-		foreach($arr as $key=>$val){ 
-		  	if($GetUsername==$val["name"] && $Getpassword==$val["url"]){
-				echo '注册成功';
-				return;
-			}else{
-				echo '注册失败';
-				return;
-			}
-		}	
+   header("content-type:text/html;charset=utf-8");
+   $GetUsername=$_POST['user'];
+   $GetPaassWWord=$_POST['password'];
+   $myInfo=array(
+       array(
+            "name"=>"zhang",
+            "pswd"=>"123333",
+       ),array(
+            "name"=>"guogege",
+            "pswd"=>"123123",
+       ),array(
+            "name"=>"wangwu",
+            "pswd"=>"456666",
+       ),array(
+            "name"=>"zhaosi",
+            "pswd"=>"258888",
+       ),
+   );
+    foreach($myInfo as $key=>$val){ 
+
+      if($GetUsername==$val["name"])
+      {
+        if($GetPaassWWord==$val["pswd"]){
+       	   $state="0";
+           break;
+        }else{
+           $state="1";
+           break;
+        };
+      }else{ 
+           $state="2";          
+      };
+    };
+    echo $state;
+  // };
 ?>
